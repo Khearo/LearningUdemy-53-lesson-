3d35f5e84ed92d9e8a7da90585da6b988d266efd
Khearo <saiaktelike@gmail.com>
2021-05-03 01:30:04 +0400

    Add project files.

---
+++ Character.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning2_53__lesson_
+{
+    class Character
+    {
+        /* Модификаторы доступа:
+            private - всегда по умолчанию. Нет доступа извне.
+            public
+            protected
+            intertal - всегда по умолчанию для класса.
+        */
+
+        public int Health = 400;  // поле здоровья.
+        
+        public void Hit(int damage, int poison )  // метод просто делает что-то. Принимает аргумент - херачит изо всех щелей. 
+        {
+            Health -= damage; // наносим урон
+            Health -= poison; // отравляем
+        }
+
+        public void Buff(int recovery, int shield) // метод накладывает бафф
+        {
+            Health += recovery; // восстанавливаем немного здоровья разово
+            Health += shield; // накладывается баф в виде щита регенерации
+        }
+
+        public void Normalize(int downHP) // метод ошеломления от излишка здоровья  
+        {
+            Health -= downHP; // нормализуем состояние здоровья.
+        }
+
+        public void Relax(string sleep)
+        {
+             // ...ZZzz
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning2_53__lesson_
+{
+    class Program2  // Udemy course cont
+    {
+        static void Main(string[] args)
+        {
+            Character c = new Character();  // экземпляр типа, через точку "." (операция доступа) идет обращение к полям класса Кэректер.
+            c.Hit(170, 33); //передаем урон и отравление.   
+            Console.WriteLine($"Ouch..Sh#t! You have {c.Health} health remaining after dealing hit");
+            
+            c.Buff(110, 100 );
+            Console.WriteLine($"\n\rWow-wow! You have {c.Health} health after recovery");
+
+            c.Normalize(7);
+            Console.WriteLine($"\n\rOkay. You have {c.Health} health after normalization health status");
+
+            Console.WriteLine("\n\rHo-ho, that's my boy! Now you are ready to adventure. But get some sleep first ;))"); // теперь ты красавчик!
+
+            Console.ReadKey();
+        }
+        
+    }
+}
